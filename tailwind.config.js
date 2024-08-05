955bcdf99bb6f171a9e555ab317f7cb1b29f4dc1
Vasily Myazin <vm@konspiredesign.com>
2024-08-05 00:06:40 -0300

    Attemp to add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layouts/**/*.html',
+    './content/**/*.md',
+    './assets/css/custom.css',
+    './themes/ink-free/static/css/main.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
